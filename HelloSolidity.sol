0368adfcdebb7c570e55de6643f8696f5310b5e1
Remix Workspace <>
2022-09-09 13:33:34 +0200

    added HelloSolidity.sol

---
+++ HelloSolidity.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+contract HelloSolidity {
+    function sayHelloSolidity() public pure returns (string memory) {
+        return "Hello Solidity";
+    }
+}
